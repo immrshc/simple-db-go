ee22f461392b2a6ee0e38ea66d36206b9c7c6219
immrshc <immrshich@gmail.com>
2024-01-20 04:41:05 +0900

    :sparkles: Implement buffer list

--- tx/buffer_list.go
+++ tx/buffer_list.go
@@ -1,1 +1,59 @@
 package tx
+
+import (
+	"slices"
+
+	"github.com/immrshc/simple-db-go/buffer"
+	"github.com/immrshc/simple-db-go/file"
+)
+
+type BufferList struct {
+	buffers map[*file.BlockID]*buffer.Buffer
+	pins    []*file.BlockID
+	bm      *buffer.Manager
+}
+
+func NewBufferList(bm *buffer.Manager) *BufferList {
+	return &BufferList{
+		bm: bm,
+	}
+}
+
+func (bl *BufferList) Buffer(blk *file.BlockID) *buffer.Buffer {
+	return bl.buffers[blk]
+}
+
+func (bl *BufferList) Pin(blk *file.BlockID) error {
+	buff, err := bl.bm.Pin(blk)
+	if err != nil {
+		return err
+	}
+	bl.buffers[blk] = buff
+	bl.pins = append(bl.pins, blk)
+	return nil
+}
+
+func (bl *BufferList) Unpin(blk *file.BlockID) error {
+	buff := bl.buffers[blk]
+	if err := bl.bm.Unpin(buff); err != nil {
+		return err
+	}
+	idx := slices.Index(bl.pins, blk)
+	bl.pins = append(bl.pins[:idx], bl.pins[idx+1:]...)
+	if !slices.Contains(bl.pins, blk) {
+		delete(bl.buffers, blk)
+	}
+	return nil
+}
+
+func (bl *BufferList) UnpinAll() error {
+	for _, blk := range bl.pins {
+		buff := bl.buffers[blk]
+		if err := bl.bm.Unpin(buff); err != nil {
+			return err
+		}
+	}
+	bl.buffers = map[*file.BlockID]*buffer.Buffer{}
+	bl.pins = make([]*file.BlockID, 0)
+	return nil
+}
